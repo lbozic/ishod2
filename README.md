add8df5839ac1ce2f3f5181330c2f6faa724bfeb
Gabrijel69 <Gabrijel.zornada@gmail.com>
2025-11-27 13:07:04 +0100

    postavljanje na git

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ishod2
+# Provjera 2. ishoda, Luka Božić 27.11.2025
+# http://localhost:9000/#/unos
---
+++ frontend/quasar-project/src/pages/UnosPage.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div class="q-pa-md q-gutter-sm">
+    <q-btn color="white" text-color="black" label="Standard" />
+    <q-btn color="primary" label="Primary" />
+    <q-btn color="secondary" label="Secondary" />
+    <q-btn color="amber" glossy label="Amber" />
+    <q-btn color="brown-5" label="Brown 5" />
+    <q-btn color="deep-orange" glossy label="Deep Orange" />
+    <q-btn color="purple" label="Purple" />
+    <q-btn color="black" label="Black" />
+    </div>
+
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
